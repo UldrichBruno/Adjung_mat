af8f243812cb78da4b150095fe83f78555d9d308
UldrichBruno <uldrich.bruno@seznam.cz>
2020-03-29 10:29:37 +0200

    Initial commit

---
+++ engine.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Uzivatel on 29.03.2020.
+//
+
---
+++ engine.h
@@ -0,0 +1,8 @@
+//
+// Created by Uzivatel on 29.03.2020.
+//
+
+#ifndef ADJUNG_MAT_ENGINE_H
+#define ADJUNG_MAT_ENGINE_H
+
+#endif //ADJUNG_MAT_ENGINE_H
