02c4d1508f911950f9b7bee0df970a6dada9161a
UldrichBruno <uldrich.bruno@seznam.cz>
2020-03-29 11:53:28 +0200

    Pokrok o další krok.

--- engine.cpp
+++ engine.cpp
@@ -16,9 +16,9 @@ void printMatrix(struct matrix a) {
     cout <<"--------------------------------------------------"<< endl;
 }
 
-struct matrix readMatrix(int inputa.size, string path) {       //Ukládání matice na vstupu.
+struct matrix readMatrix(int size, string path) {       //Ukládání matice na vstupu.
     struct matrix a;
-    a.size = inputa.size;
+    a.size = size;
     int x;
     ifstream inFile;
     inFile.open(path);
@@ -42,15 +42,15 @@ bool check(struct matrix a, int line, int col) {
     return a.array[line][col] != 0;
 }
 
-bool checkSingular(int numOfSteps, int DIM){
-    return numOfSteps<DIM+3;
+bool checkSingular(int numOfSteps, int inputDIM){
+    return numOfSteps<inputDIM+3;
 }
 
 void zeroingElement(struct matrix a, int lineNullHead, int lineReadHead, float coef) {            //Vynuluje číslo pod diagonálou v závislosti na umístění čtecí hlavy.
     for (int i = 0; i < a.size; i++) {
         a.array[lineNullHead][i] = a.array[lineNullHead][i] + a.array[lineReadHead][i] * coef;
     }
-    numberOfstep++;
+  //  numberOfstep++;
 }
 
 double findCoef(struct matrix a, int lineNullHead, int lineReadHead) {
@@ -58,14 +58,25 @@ double findCoef(struct matrix a, int lineNullHead, int lineReadHead) {
     return coef = (-1 * (a.array[lineNullHead][lineReadHead] / a.array[lineReadHead][lineReadHead]));
 }
 
-void zeroingElement(struct matrix a, int lineNullHead, int lineReadHead ,float coef) {            //Vynuluje číslo pod diagonálou v závislosti na umístění čtecí hlavy.
 
-    for (int i = 0; i < a.size; i++) {
-        a.array[lineNullHead][i] = a.array[lineNullHead][i] + a.array[lineReadHead][i] * coef;
+void moveLine(struct matrix a, int lineReadHead) {        //Odsun řádku kvůli nevyhovujícímu nulovému prvku pod čtecí hlavou na poslední řádek.
+
+    double opmatrix[MAX_SIZE_OF_MATRIX];
+
+    for (int k = 0; k <a.size; k++) {
+        opmatrix[k] = a.array[lineReadHead][k];
     }
-    numberOfstep++;
-}
 
+    for (int i = lineReadHead; i < a.size; i++) {
+        for (int k = 0; k < a.size; k++) {
+            a.array[i][k] = a.array[i + 1][k];
+        }
+    }
+
+    for (int k = 0; k < a.size; k++) {
+        a.array[a.size][k] = opmatrix[k];
+    }
+}
 
 struct matrix HST(struct matrix a) {             //Převod do horního stupňovitého tvaru.
     int numOfLoops = 0;
@@ -73,7 +84,7 @@ struct matrix HST(struct matrix a) {             //Převod do horního stupňovi
     for (int i = 0; i < a.size; i++) {         //Čtecí hlava.
         if (check(i, i) == 1) {
             for (int x = i + 1; x < a.size; x++) {    //Nulovací hlava.
-                zeroingElement(x, i);
+                zeroingElement(struct matrix origin, x, i, float coef);
             }
         } else {
             numOfLoops++;
